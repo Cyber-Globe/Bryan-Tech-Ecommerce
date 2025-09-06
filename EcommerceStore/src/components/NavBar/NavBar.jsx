e8f950711d0497e76c2939969f0fdcdbe92c1eb1
cyber <ojeikelvin@gmail.com>
2025-09-06 05:51:15 +0100

    completed the desktop nav section of my ecommere web application

--- EcommerceStore/src/components/NavBar/NavBar.jsx
+++ EcommerceStore/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+// import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
 import "./NavBar.css";
 
@@ -7,33 +7,58 @@ function NavBar() {
   const [menu, setMenu] = React.useState("shop");
 
   return (
-    <div className="nav-bar">
-      <div className="nav-logo">
-        <img src={logo} alt="logo" />
-        <p>TECH-SHOP</p>
+    <>
+      <div className="discription">
+        <div></div>
+        <div>
+          <p>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!{" "}
+            <span>Shop Now</span>
+          </p>
+        </div>
+        <div>
+          English <i class="fa fa-caret-down"></i>
+        </div>
       </div>
+      <div className="nav-bar">
+        <div className="nav-logo">
+          <p>Exclusive</p>
+        </div>
 
-      <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}>
-          Shop {menu === "shop" ? <hr /> : ""}
-        </li>
-        <li onClick={() => setMenu("phone")}>
-          Phones {menu === "phone" ? <hr /> : ""}
-        </li>
-        <li onClick={() => setMenu("laptop")}>
-          Laptops {menu === "laptop" ? <hr /> : ""}
-        </li>
-        <li onClick={() => setMenu("gadget")}>
-          Gadget {menu === "gadget" ? <hr /> : ""}
-        </li>
-      </ul>
+        <ul className="nav-menu">
+          <li onClick={() => setMenu("home")}>
+            Home {menu === "home" ? <hr /> : ""}
+          </li>
+          <li onClick={() => setMenu("contact")}>
+            Contact {menu === "contact" ? <hr /> : ""}
+          </li>
+          <li onClick={() => setMenu("about")}>
+            About {menu === "about" ? <hr /> : ""}
+          </li>
+          <li onClick={() => setMenu("signup")}>
+            Signup {menu === "signup" ? <hr /> : ""}
+          </li>
+        </ul>
+        <div className="nav-search-container">
+          <div className="search-container">
+            <input
+              type="search"
+              class="search-box"
+              placeholder="What are you looking for?"
+            ></input>
+            <i class="fa fa-search"></i>
+          </div>
+          <i class="fa fa-heart"></i>
+          <img src={cart_icon} alt="cart" />
+        </div>
 
-      <div className="nav-login-cart">
+        {/* <div className="nav-login-cart">
         <button className="login-btn">Login</button>
         <img src={cart_icon} alt="cart" />
         <div className="nav-cart-count">0</div>
+      </div> */}
       </div>
-    </div>
+    </>
   );
 }
 
